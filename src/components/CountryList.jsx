22dec9211509da24a67f3f2e5ffa47b458a7dabc
iKeyFx <techchev@gamil.com>
2024-06-29 13:29:56 +0100

    changes

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,6 +1,6 @@
 
 import CountryItem from './CountryItem'
-import styles from './countryList.module.css'
+import styles from './CountryList.module.css'
 import Spinner from "./Spinner" 
 import Message from "./Message"
 import { useCities } from '../contexts/CitiesContext';
